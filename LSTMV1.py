995ebec0e98f5b4cac1b74ab562e0ac7e855d727
DiNo <brandino0610@gmail.com>
2019-11-01 00:30:33 -0500

    Possibly ready to train without batching

--- LSTMV1.py
+++ LSTMV1.py
@@ -3,12 +3,13 @@ import tensorflow as tf
 import numpy as np
 
 # Hyper parameters
-batch_size = 55
+BATCH_SIZE = 55
+EPOCHS = 5
 
 # Data - needs to be in shape of 3d tensor made up of slices of 2d array
 btc_data = pd.read_csv('coindata/BTCData.csv')
 btc_data = btc_data.select_dtypes(include='float')
-state_size = len(btc_data.columns)
+STATE_SIZE = len(btc_data.columns)
 x_test = btc_data[:300]
 x_train = btc_data[300:]
 
@@ -18,7 +19,6 @@ x_train = tf.constant(x_train.to_numpy(), tf.float32)
 
 # Labels need to represent output data
 # Output will be candle trend data 1 for positive and 0 for negative
-
 labels = []
 for index, row in btc_data.iterrows():
     if row['Open'] < row['Close']:
@@ -34,22 +34,21 @@ y_train = labels[300:]
 y_test = tf.constant(y_test, tf.int8)
 y_train = tf.constant(y_train, tf.int8)
 
+# Batch all data
+x_test = tf.strided_slice(x_test, strides=BATCH_SIZE)
+print(x_test)
+
 # Build Model
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.LSTM(770, input_shape=[batch_size, state_size]))
+model.add(tf.keras.layers.LSTM(770, input_shape=[BATCH_SIZE, STATE_SIZE, 1]))
+
 model.add(tf.keras.layers.Dense(21, activation='softmax'))
 model.build()
-model.summary()
-
 
 # Compile model
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+model.compile(optimizer=tf.keras.optimizers.Adagrad(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy())
 
-# # Fit model
-# # model.fit(x_train, y_train,
-#           validation_set=(x_test, y_test),
-#           batch_size=batch_size,
-#           epochs=5)
+# Fit model
+model.fit((x_train, y_train), epochs=EPOCHS, validation_data=(x_test, y_test))
 
