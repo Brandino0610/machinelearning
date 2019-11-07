8bf0ce8bfd37edc2267b95923ef2782b94ea7d76
brandon <brandino0610@gmail.com>
2019-11-06 19:25:26 -0600

    Work done from school

--- LSTMV1.py
+++ LSTMV1.py
@@ -17,14 +17,11 @@ x_train = btc_data[BATCH_SIZE * 6:btc_data.__len__() - (btc_data.__len__() % BAT
 X_TEST_DEPTH = int(x_test.__len__() / BATCH_SIZE)
 X_TRAIN_DEPTH = int(x_train.__len__() / BATCH_SIZE)
 
-# Convert to tensors
-x_test = tf.Variable(x_test.to_numpy(), tf.float64)
-x_train = tf.Variable(x_train.to_numpy(), tf.float64)
 
 # Labels need to represent output data
 # Output will be candle trend data 1 for positive and 0 for negative
 labels = []
-for index, row in btc_data.iterrows():
+for index, row in x_train.iterrows():
     if row['Open'] < row['Close']:
         labels.append(1)
     else:
@@ -33,14 +30,19 @@ for index, row in btc_data.iterrows():
 # Split labels for testing and training
 y_test = labels[:330]
 y_train = labels[330:]
+Y_TRAIN_DEPTH = labels.__len__() / BATCH_SIZE
 
 # Convert to tensors
-y_test = tf.constant(y_test, tf.int8)
-y_train = tf.constant(y_train, tf.int8)
+y_test = tf.Variable(y_test, tf.int64)
+y_train = tf.Variable(y_train, tf.int64)
+x_test = tf.Variable(x_test.to_numpy(), tf.float64)
+x_train = tf.Variable(x_train.to_numpy(), tf.float64)
+
 
 # Batch all data
 # x_test = tf.reshape(x_test, [X_TEST_DEPTH, BATCH_SIZE, STATE_SIZE])
 x_train = tf.reshape(x_train, [X_TRAIN_DEPTH, BATCH_SIZE, STATE_SIZE])
+y_train = tf.reshape(y_train, [Y_TRAIN_DEPTH, BATCH_SIZE])
 
 # Build Model
 model = tf.keras.Sequential()
@@ -53,5 +55,8 @@ model.compile(optimizer=tf.keras.optimizers.Adagrad(),
               loss=tf.keras.losses.SparseCategoricalCrossentropy())
 
 # Fit model
-model.fit((x_train, y_train), epochs=EPOCHS, validation_data=(x_test, y_test))
+model.fit(x_train, y_train,
+          validation_data=(x_test, y_test),
+          batch_size=BATCH_SIZE,
+          epochs=EPOCHS)
 
