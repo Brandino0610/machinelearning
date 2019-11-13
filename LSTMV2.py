4c5d3828df7b19c9c61fe9d32c84166659104be4
DiNo <brandino0610@gmail.com>
2019-11-13 00:25:15 -0600

    Got multi-step data working. Need to come up with better architecture possibly.

--- LSTMV2.py
+++ LSTMV2.py
@@ -6,60 +6,81 @@ import numpy as np
 
 # Hyper parameters
 BATCH_SIZE = 21
-EPOCHS = 5
+EPOCHS = 3
 BUFFER_SIZE = 55
+TEST_SET_SIZE = 400
 
 # Data - needs to be in shape of 3d tensor made up of slices of 2d array
 btc_data = pd.read_csv('coindata/BTCData.csv')
 btc_data = btc_data.select_dtypes(include='float')
-STATE_SIZE = len(btc_data.columns)
+STATE_SIZE = len(btc_data.columns)  # subtract one column for labels
 TOTAL_LENGTH = btc_data.__len__() - BUFFER_SIZE
-btc_data_step = np.empty([TOTAL_LENGTH, BUFFER_SIZE, STATE_SIZE])
 
+label_data = []
+for index, row in btc_data.iterrows():
+    if row['Open'] < row['Close']:
+        label_data.append(1)
+    else:
+        label_data.append(0)
+
+
+label_data_step = np.empty([TOTAL_LENGTH, BUFFER_SIZE])
+btc_data_step = np.empty([TOTAL_LENGTH, BUFFER_SIZE, STATE_SIZE])
 for i in range(TOTAL_LENGTH):
     btc_data_step[i] = btc_data[i:i + BUFFER_SIZE].to_numpy()
+    label_data_step[i] = label_data[i:i + BUFFER_SIZE]
 
 
+x_train = btc_data_step[:len(btc_data_step) - TEST_SET_SIZE, :, :]
+x_test = btc_data_step[len(btc_data_step) - TEST_SET_SIZE:, :, :]
+y_train = label_data_step[:len(label_data_step) - TEST_SET_SIZE]
+y_test = label_data_step[len(label_data_step) - TEST_SET_SIZE:]
+
+x_train = tf.constant(x_train, tf.float64)
+x_test = tf.constant(x_test, tf.float64)
+y_train = tf.constant(y_train, tf.float64)
+y_test = tf.constant(y_test, tf.float64)
 
 
-btc_data_step = tf.constant(btc_data_step, tf.float64)
-print(tf.shape(btc_data_step))
+# Build Model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.LSTM(770, input_shape=[BUFFER_SIZE, STATE_SIZE]))
+model.add(tf.keras.layers.Dense(128))
+model.add(tf.keras.layers.Dense(BUFFER_SIZE))
+model.build()
+
+# Compile model
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss=tf.keras.losses.CategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+# Fit model
+single_step_history = model.fit(x_train, y_train,
+                                validation_data=(x_test, y_test),
+                                batch_size=BATCH_SIZE,
+                                epochs=EPOCHS,)
+
+prediction_values = tf.constant(btc_data_step[765], tf.float64)
+prediction_values = tf.reshape(prediction_values, [1, BUFFER_SIZE, STATE_SIZE])
+
+model.predict(tf.constant(prediction_values))
 
 
-#
-# # Build Model
-# model = tf.keras.Sequential()
-# model.add(tf.keras.layers.LSTM(770, input_shape=[BUFFER_SIZE, STATE_SIZE]))
-# model.add(tf.keras.layers.Dense(256))
-# model.add(tf.keras.layers.Dense(55))
-# model.build()
-#
-# # Compile model
-# model.compile(optimizer=tf.keras.optimizers.Adam(),
-#               loss=tf.keras.losses.CategoricalCrossentropy())
-#
-# # Fit model
-# single_step_history = model.fit(x_train, y_train,
-#                                 validation_data=(x_test, y_test),
-#                                 batch_size=BATCH_SIZE,
-#                                 epochs=EPOCHS)
-#
-#
-# def plot_train_history(history, title):
-#     loss = history.history['loss']
-#     val_loss = history.history['val_loss']
-#
-#     epochs = range(len(loss))
-#
-#     plt.figure()
-#
-#     plt.plot(epochs, loss, 'b', label='Training loss')
-#     plt.plot(epochs, val_loss, 'r', label='Validation loss')
-#     plt.title(title)
-#     plt.legend()
-#
-#     plt.show()
-#
-#
-# plot_train_history(single_step_history,
-#                    'Single Step History and Validation')
+def plot_train_history(history, title):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(loss))
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title(title)
+    plt.legend()
+
+    plt.show()
+
+
+plot_train_history(single_step_history,
+                   'Single Step History and Validation')
